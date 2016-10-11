e1a6f95c11e69126a87ccfa9732bf57f83e73f9f
JuergenGutsch <juergen@gutsch-online.de>
2016-10-11 23:04:13 +0200

    new angular2 app on VS2015

---
+++ src/WebApplication2/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404
+                    && !Path.HasExtension(context.Request.Path.Value))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+
+            app.UseStaticFiles();
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ src/WebApplication2/gulpfile.js
@@ -0,0 +1,61 @@
+﻿var gulp = require('gulp');
+
+var libs = './wwwroot/libs/';
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+gulp.task('restore:core-js', function() {
+    gulp.src([
+        'node_modules/core-js/client/*.js'
+    ]).pipe(gulp.dest(libs + 'core-js'));
+});
+gulp.task('restore:zone.js', function () {
+    gulp.src([
+        'node_modules/zone.js/dist/*.js'
+    ]).pipe(gulp.dest(libs + 'zone.js'));
+});
+gulp.task('restore:reflect-metadata', function () {
+    gulp.src([
+        'node_modules/reflect-metadata/reflect.js'
+    ]).pipe(gulp.dest(libs + 'reflect-metadata'));
+});
+gulp.task('restore:systemjs', function () {
+    gulp.src([
+        'node_modules/systemjs/dist/*.js'
+    ]).pipe(gulp.dest(libs + 'systemjs'));
+});
+gulp.task('restore:rxjs', function () {
+    gulp.src([
+        'node_modules/rxjs/**/*.js'
+    ]).pipe(gulp.dest(libs + 'rxjs'));
+});
+gulp.task('restore:angular-in-memory-web-api', function () {
+    gulp.src([
+        'node_modules/angular-in-memory-web-api/**/*.js'
+    ]).pipe(gulp.dest(libs + 'angular-in-memory-web-api'));
+});
+
+gulp.task('restore:angular', function () {
+    gulp.src([
+        'node_modules/@angular/**/*.js'
+    ]).pipe(gulp.dest(libs + '@angular'));
+});
+
+gulp.task('restore:bootstrap', function () {
+    gulp.src([
+        'node_modules/bootstrap/dist/**/*.*'
+    ]).pipe(gulp.dest(libs + 'bootstrap'));
+});
+
+gulp.task('restore', [
+    'restore:core-js',
+    'restore:zone.js',
+    'restore:reflect-metadata',
+    'restore:systemjs',
+    'restore:rxjs',
+    'restore:angular-in-memory-web-api',
+    'restore:angular',
+    'restore:bootstrap'
+]);
